3a67f45c3e0962c95929e4ed11caea426c330626
ad105geppetto <ad105geppetto@gmail.com>
2023-05-17 19:23:13 +0900

    fix: 유틸 함수 blurInput 수정

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,9 @@
 import { FormRef, InputRef } from "../types/todo";
 
 export const blurInput = (ref: InputRef, formRef: FormRef) => {
-  const { scrollWidth, clientWidth } = ref.current!;
+  if (!ref.current || !formRef.current) return;
+  const { scrollWidth, clientWidth } = ref.current;
   if (scrollWidth > clientWidth && scrollWidth > 290)
-    formRef.current!.style.boxShadow = '0px 4px 4px rgba(0, 0, 0, 0.25)';
-  else formRef.current!.style.boxShadow = 'none';
+    formRef.current.style.boxShadow = "0px 4px 4px rgba(0, 0, 0, 0.25)";
+  else formRef.current.style.boxShadow = "none";
 };
