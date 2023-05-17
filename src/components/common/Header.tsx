3b80582f993bbd69881d785c1ffb166b0835f2c4
soohyuneee <tlstngus95@naver.com>
2023-05-17 16:51:32 +0900

    refact: Header className, css 속성 fix

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -3,7 +3,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <header className="header-style">
-      <h1 className=".title-style">Toodos</h1>
+      <h1 className="title-style">Toodos</h1>
     </header>
   );
 };
