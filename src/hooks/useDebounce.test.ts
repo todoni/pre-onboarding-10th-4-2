378fe1b20f544fbdfe739f87ef83536d9ae040da
hwangsungjae <edx2fire@likelion.org>
2023-05-17 17:46:12 +0900

    test : write test code for hooks

--- src/hooks/useDebounce.test.ts
+++ src/hooks/useDebounce.test.ts
@@ -1,11 +1,39 @@
-import { renderHook } from "@testing-library/react-hooks";
-import useDebounce from "./useDebounce";
+import { renderHook } from '@testing-library/react-hooks';
+import useDebounce from './useDebounce';
 
-describe("useDebounce", () => {
-  test("useDebounce는 함수를 리턴한다", () => {
-    const { result } = renderHook(() => useDebounce(500));
-    const debounce = result.current;
+jest.useFakeTimers();
 
-    expect(typeof debounce).toBe("function");
+describe('useDebounce', () => {
+  it('should call callback function after certain time', () => {
+    const time = 500;
+    const { result } = renderHook(() => useDebounce(time));
+
+    const fnc = jest.fn();
+
+    result.current(fnc);
+
+    jest.advanceTimersByTime(time - 100);
+    expect(fnc).not.toHaveBeenCalled();
+
+    jest.advanceTimersByTime(100);
+    expect(fnc).toHaveBeenCalledTimes(1);
+  });
+
+  it('should debounce the previous function with clearTimeout', () => {
+    const time = 500;
+    const { result } = renderHook(() => useDebounce(time));
+
+    const fnc1 = jest.fn();
+    const fnc2 = jest.fn();
+
+    result.current(fnc1);
+    jest.advanceTimersByTime(time - 100);
+    result.current(fnc2);
+    
+    jest.advanceTimersByTime(100);
+    expect(fnc1).not.toHaveBeenCalled();
+    
+    jest.advanceTimersByTime(time - 100);
+    expect(fnc2).toHaveBeenCalledTimes(1);
   });
 });
